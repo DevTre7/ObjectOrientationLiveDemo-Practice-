4c98b56d84bf3a957afcaff63bc86ef2d67aee0d
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2022-11-08 20:05:05 -0600

    first commit

---
+++ objectOrientationLiveDemo.js
@@ -0,0 +1,9 @@
+let person = {
+  firstName: "Chandler",
+  lastName: "Bong",
+  age: 34,
+};
+
+console.log("Dot Notation: ", person.firstName);
+console.log("Bracket Notations: ", person["firstName"]);
+console.log("hello");
